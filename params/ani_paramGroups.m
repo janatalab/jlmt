536478403571b1e1dd1b7bf75723fcd5accf4218
petr <petr@ucdavis.edu>
2012-10-31 06:47:03 +0000

    jlmt rearrangements
    git-svn-id: https://atonal.ucdavis.edu/repos/private/matlab/jlmt@2212 28c61866-8173-4a13-8a39-e4c4ff750e15

---
+++ params/ani_paramGroups.m
@@ -0,0 +1,15 @@
+function ani = ani_paramGroups
+% Returns ani parameter structure commonly used for BTB algorithm
+%
+% Copyright (c) 2007-2012 The Regents of the University of California
+% All Rights Reserved
+%
+% Author(s):
+% Stefan Tomic
+
+
+ani.PlotFlag = 0;
+ani.DownSampleFactor = 5;
+ani.normalize_wav = 1;
+ani.start_time_sec = [];
+ani.dur_sec = [];
