224a199290a99af57f295595efd13750cfb1be7c
fbarrett <fbarrett@ucdavis.edu>
2012-11-06 22:04:14 +0000

    updting jlmt_startup.m
    git-svn-id: https://atonal.ucdavis.edu/repos/private/matlab/jlmt@2253 28c61866-8173-4a13-8a39-e4c4ff750e15

--- jlmt_startup.m
+++ jlmt_startup.m
@@ -1,31 +1,25 @@
 % startup file entries for the JLMT
 % 
-% If you do not currently have a 'startup.m' file in your
-% /home/%username%/matlab directory (in Linux) or your
-% C:\Documents\MATLAB\work directory (in Windows), or if that file exists
-% and is empty, you can simply copy this file there and rename it
-% 'startup.m'. It will then be run every time you open MATLAB, and it will
-% set these paths and make the JLMT, IPEMToolbox, and mp3read available. If
-% a 'startup.m' file exists and has paths in it already, you can simply
-% copy the following lines into your existing 'startup.m' file and edit
-% them accordingly. Either way, you must restart MATLAB for the changes to
-% take effect. To test whether your paths have been set properly, execute:
-%   >> which jlmt_proc_series.m
-% This should return the path to your JLMT installation, with the 'proc'
-% directory appended to it ('/path/to/jlmt/proc').
+% After checking out the JLMT repository, add a line to your startup.m
+% file adding the top-level directory of the repository to the path, and
+% then add this script on the following line and restart Matlab. This
+% will set the paths necessary to run JLMT.
 % 
-% FB 2012.07.11
+% FB 2012.11.06
 
-% % PATH TO JLMT : replace '/path/to/jlmt_dist' with the path to the JLMT
-% and uncomment the following line
-% path(path,genpath('/path/to/jlmt_dist'));
+fprintf(1,'Adding JLMT paths ...');
+
 jlmtpath = fileparts(which('jlmt_proc_series'));
-addpath(fullfile(jlmtpath,'maps'));
-addpath(fullfile(jlmtpath,'params'));
+
+addpath(fullfile(jlmtpath,'event_objects'));
 addpath(fullfile(jlmtpath,'includes'));
+addpath(fullfile(jlmtpath,'maps'));
+addpath(fullfile(jlmtpath,'midi'));
+addpath(fullfile(jlmtpath,'params'));
+addpath(fullfile(jlmtpath,'plots'));
+addpath(fullfile(jlmtpath,'proc'));
 addpath(fullfile(jlmtpath,'rp_modules'));
+addpath(fullfile(jlmtpath,'test'));
+addpath(fullfile(jlmtpath,'utils'));
 
-% % PATH TO IPEM Toolbox : replace '/path/to/IPEMToolbox' with the path to
-% % your IPEM Toolbox installation and uncomment the following line
-%path(path,genpath('/path/to/IPEMToolbox'));
-
+fprintf(1,'\n');
