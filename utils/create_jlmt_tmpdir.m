73eb5898e1f326948eae8bf960b470bfeeb8235a
fbarrett <fbarrett@ucdavis.edu>
2013-01-09 19:29:27 +0000

    added re-try mechanism to create_jlmt_tmpdir. This was erroring when executed multiple times at the same clock time within the parallel computing workspace. I added re-try mechanisms and random pauses (which also do not collide in the parallel space).
    git-svn-id: https://atonal.ucdavis.edu/repos/private/matlab/jlmt@2360 28c61866-8173-4a13-8a39-e4c4ff750e15

--- utils/create_jlmt_tmpdir.m
+++ utils/create_jlmt_tmpdir.m
@@ -16,6 +16,12 @@ function tmpdir = create_jlmt_tmpdir(tmpdir)
 % Author(s):
 % 12/10/06 Petr Janata
 % 2012.07.05 FB - updated for JLMT
+% 2013.01.09 FB - added retry mechanism with random pauses between tries to accomodate
+%   parallel computing environments. Previously, the same clock time was generated
+%   by multiple jobs that were distributed, leading this script to try to create an
+%   already existing tmpdir. The re-try mechanism places randomly spaced pauses. These
+%   random pauses do not collide in parallel computing space. Rand() (without seeding)
+%   will generate different random numbers for each worker.
 
 % Make sure we have the basic root directory
 dir_root = '/tmp/jlmt/';
